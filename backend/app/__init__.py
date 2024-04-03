d15ab89941006e325d6abf075b83a1ba114232c6
Arav Kumar <68564296+A-Ravioli@users.noreply.github.com>
2024-04-03 16:57:18 -0700

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,1 +1,17 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy as SQL
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finderdb.sqlite3'  # our database connection string
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+db = SQL(app)
+
+# Import routes after initializing db to avoid circular imports
+from . import routes
+
+# Initialize the database
+@app.before_first_request
+def create_tables():
+    db.create_all()
 
