59a780b32b0759873acaad53c5a1b8947add26be
Arav Kumar <68564296+A-Ravioli@users.noreply.github.com>
2024-04-03 17:39:03 -0700

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,1 +1,9 @@
+# Optional File
+import os
+
+class Config:
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///finderdb.sqlite3'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
