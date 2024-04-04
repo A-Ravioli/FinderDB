b4339aa08964aab650f3f8598546fbf936b8a0e7
Arav Kumar <68564296+A-Ravioli@users.noreply.github.com>
2024-04-03 17:39:43 -0700

    Update __init__.py

--- backend/app/__init__.py
+++ backend/app/__init__.py
@@ -1,14 +1,15 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy as SQL
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from config import Config
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///finderdb.sqlite3'  # our database connection string
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(Config)
 
-db = SQL(app)
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
 
-# Import routes after initializing db to avoid circular imports
-from . import routes
+from app import routes, models
 
 # Initialize the database
 @app.before_first_request
