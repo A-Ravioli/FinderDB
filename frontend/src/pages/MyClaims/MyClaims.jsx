18fce63b9c8566ffa114073d79861401764ad4d7
Nathan Cheung <nathancheungchess@gmail.com>
2024-04-18 14:39:16 -0700

    No claims message

--- frontend/src/pages/MyClaims/MyClaims.jsx
+++ frontend/src/pages/MyClaims/MyClaims.jsx
@@ -10,12 +10,17 @@ function MyClaims() {
   });
 
   return (
-    <div id={MyClaimsCSS["items-container"]}>
-      <h1>Claimed items</h1>
-      {items?.map((itemData) => (
-        <ItemCard itemData={itemData} />
-      ))}
-    </div>
+    <div id={MyClaimsCSS['items-container']}>
+    <h1>Claimed items</h1>
+    {isLoading ? (
+      <p>Loading...</p>
+    ) : items && items.length ? (
+      items.map((itemData) => <ItemCard key={itemData.id} itemData={itemData} />)
+    ) : (
+      <p>No claims currently</p>
+    )}
+  </div>
+
   );
 }
 
