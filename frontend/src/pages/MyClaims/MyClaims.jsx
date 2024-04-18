bce97374f531846464541e2cc0555ea1edd9e296
Nathan Cheung <nathancheungchess@gmail.com>
2024-04-18 14:50:05 -0700

    My claims button fixed

--- frontend/src/pages/MyClaims/MyClaims.jsx
+++ frontend/src/pages/MyClaims/MyClaims.jsx
@@ -9,12 +9,12 @@ function MyClaims() {
     queryFn: getClaimedItems,
   });
 
+  console.log(items)
+
   return (
     <div id={MyClaimsCSS['items-container']}>
     <h1>Claimed items</h1>
-    {isLoading ? (
-      <p>Loading...</p>
-    ) : items && items.length ? (
+    {items?.length !== 0 ? (
       items.map((itemData) => <ItemCard key={itemData.id} itemData={itemData} />)
     ) : (
       <p>No claims currently</p>
